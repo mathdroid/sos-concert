b4a3774d61d7b32b04ed32742a25c74bf3cdbb59
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2019-10-21 13:01:07 +0700

    enable offline

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
     runtimeCaching: [
       {
         urlPattern: /^https?.*/,
-        handler: "NetworkFirst",
+        handler: "CacheFirst",
         options: {
           cacheName: "https-calls",
           networkTimeoutSeconds: 15,
